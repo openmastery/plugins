29285e158cfd5d5273b96b900f68bdffa9ccfcb0
Janelle Klein <janelle@newiron.com>
2013-12-31 16:45:45 -0600

    remove hibernate stuff

--- visualizer/grails-app/conf/BuildConfig.groovy
+++ visualizer/grails-app/conf/BuildConfig.groovy
@@ -40,12 +40,9 @@ grails.project.dependency.resolution = {
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-
-        // runtime 'mysql:mysql-connector-java:5.1.22'
     }
 
     plugins {
-        runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.8.3"
         runtime ":resources:1.2"
 
@@ -55,9 +52,6 @@ grails.project.dependency.resolution = {
         //runtime ":yui-minify-resources:0.1.5"
 
         build ":tomcat:$grailsVersion"
-
-        runtime ":database-migration:1.3.2"
-
         compile ':cache:1.0.1'
     }
 }
