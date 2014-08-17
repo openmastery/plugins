fb9013b25095f816a1046ebc7039d92677b4827f
Mike Lueders <mike@roadkill.org>
2014-08-17 17:37:56 -0500

    moved vagrant files from here to github ideaflow visualizer repo

--- vagrant/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt-get -y install openjdk-7-jre-headless
-
-curl -s http://ideaflow.org/visualizer-0.9.jar > visualizer.jar
-
-java -jar visualizer.jar context=visualizer &
-
-sleep 20
