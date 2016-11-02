1b112ad815d0d106583ac138485a3ce9227d35bd
janelle <janelle@newiron.com>
2016-11-02 14:46:33 -0500

    Update icons, buttons, and UI text, still persisting notes of same type.

---
+++ intellij-plugin/src/main/groovy/com/ideaflow/intellij/action/ifm/CreateAwesomeNote.groovy
@@ -0,0 +1,26 @@
+package com.ideaflow.intellij.action.ifm
+
+import com.ideaflow.controller.IFMController
+import com.ideaflow.intellij.action.ActionSupport
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import org.openmastery.ideaflow.intellij.IdeaFlowApplicationComponent
+
+@Mixin(ActionSupport)
+class CreateAwesomeNote extends AnAction {
+
+	@Override
+	void actionPerformed(AnActionEvent e) {
+		IFMController controller = IdeaFlowApplicationComponent.getIFMController()
+
+		String note = IdeaFlowApplicationComponent.promptForInput("Let's Celebrate!", "What did you figure out?")
+		controller.addNote(note)
+	}
+
+	@Override
+	public void update(AnActionEvent e) {
+		super.update(e);
+		disableWhenNotRecording(e)
+	}
+
+}
---
+++ intellij-plugin/src/main/groovy/com/ideaflow/intellij/action/ifm/CreateWTFNote.groovy
@@ -0,0 +1,26 @@
+package com.ideaflow.intellij.action.ifm
+
+import com.ideaflow.controller.IFMController
+import com.ideaflow.intellij.action.ActionSupport
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import org.openmastery.ideaflow.intellij.IdeaFlowApplicationComponent
+
+@Mixin(ActionSupport)
+class CreateWTFNote extends AnAction {
+
+	@Override
+	void actionPerformed(AnActionEvent e) {
+		IFMController controller = IdeaFlowApplicationComponent.getIFMController()
+
+		String note = IdeaFlowApplicationComponent.promptForInput("WTF is THIS?!", "What's the observation you don't understand? (Why Question)")
+		controller.addNote(note)
+	}
+
+	@Override
+	public void update(AnActionEvent e) {
+		super.update(e);
+		disableWhenNotRecording(e)
+	}
+
+}
